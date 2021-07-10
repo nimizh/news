8a0cfe5054e085fb70ddd1c0949ae52a473c2ffd
nimizh <86304541+nimizh@users.noreply.github.com>
2021-07-10 23:14:22 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("Your javascript has been linked");
+// 7f028a0e7c7249f2b0610c27cf525e36
+let sources = "the-wall-street-journal";
+let apiKey = "7f028a0e7c7249f2b0610c27cf525e36";
+
+let newsAccord = document.getElementById("newsAccord");
+
+const xhr = new XMLHttpRequest();
+xhr.open(
+  "GET",
+  `https://newsapi.org/v2/top-headlines?sources=${sources}&apiKey=${apiKey}`,
+  true
+);
+
+xhr.onload = function () {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles);
+
+    let html = "";
+
+    articles.forEach(function (elements, index) {
+      let news = ` <div class="card">
+                         <div class="card-header" id="heading${index}">
+                          <h5 class="mb-0">
+                           <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse${index}"
+                            aria-expanded="true" aria-controls="collapse${index}">
+                            ${elements["title"]}
+                            </button>
+                           </h5>
+                        </div>
+
+                         <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccord">
+                             <div class="card-body">
+                                ${elements["content"]}. <a href="${elements["url"]}" target = "blank" >Click here for more. </a>
+                             </div>
+                          </div>
+                      </div>
+              `;
+      html += news;
+    });
+    newsAccord.innerHTML = html;
+  } else {
+    console.log("not parsed error");
+  }
+};
+xhr.send();
